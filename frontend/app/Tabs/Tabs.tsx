6aa0609745549160bac85c32aa6b95e4606703af
ExoticOnix <mshapirovskyy@gmail.com>
2024-11-01 17:01:23 +0200

    Added icons.

--- frontend/app/Tabs/Tabs.tsx
+++ frontend/app/Tabs/Tabs.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { Folder, Settings } from 'lucide-react';
 
 export const Tabs = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px" }}>
-            <Button>Button 1</Button>
-            <Button>Button 2</Button>
-            <Button>Button 3</Button>
+            <button style={{color: "white"}}><Folder /></button>
+            <button style={{ color: "white" }}><Settings /></button>
         </div>
     );
 };

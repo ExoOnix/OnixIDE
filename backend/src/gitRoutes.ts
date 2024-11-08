84871b919c5354ea8510f68a6dbe56fdf9df99f7
ExoticOnix <mshapirovskyy@gmail.com>
2024-11-08 11:56:43 +0200

    added staging files

--- backend/src/gitRoutes.ts
+++ backend/src/gitRoutes.ts
@@ -1,13 +1,17 @@
 // socketHandlers.ts
 import { simpleGit, SimpleGit, CleanOptions } from 'simple-git';
 import { Socket } from 'socket.io';
-
+import { existsSync } from 'fs';
 const git: SimpleGit = simpleGit('./project');
 
 export async function generalChange(io: any) {
     try {
+        let GitRunning = false
+        if (existsSync("./project/.git")) {
+            GitRunning = true
+        }
         const status = await git.status();
-        io.emit("gitUpdate", status)
+        io.emit("gitUpdate", status, GitRunning)
     } catch (err) {
         console.error(err)
     }
@@ -16,6 +20,14 @@ export async function generalChange(io: any) {
 // Example of the 'updateTree' handler
 export function GitRoutes(socket: Socket, io: any) {
     socket.on('recieveGit', () => {
+        generalChange(io)
+    })
+    socket.on("addFile", (filePath) => {
+        git.add(filePath)
+        generalChange(io)
+    })
+    socket.on("resetFile", (filePath) => {
+        git.reset([filePath])
         generalChange(io)
     })
 }
--- frontend/app/Git/Git.tsx
+++ frontend/app/Git/Git.tsx
@@ -13,14 +13,16 @@ export const Git = () => {
     const socket: any = useSocket();
     const [unstagedFiles, setUnstagedFiles] = useState<string[]>([]);
     const [stagedFiles, setStagedFiles] = useState<string[]>([]);
+    const [gitRunning, setGitRunning] = useState<boolean>(false);
+
     useEffect(() => {
         socket.emit('recieveGit')
     }, [socket])
     useEffect(() => {
-        socket.on("gitUpdate", (status: GitStatus) => {
+        socket.on("gitUpdate", (status: GitStatus, gitRunning: boolean) => {
             console.log("Status received:", status);
 
-
+            setGitRunning(gitRunning);
             setUnstagedFiles(status.not_added || []);
             setStagedFiles(status.staged || []); // Assuming 'staged' is available as an array in the status object
         });
@@ -29,6 +31,13 @@ export const Git = () => {
             socket.off("gitUpdate");
         };
     }, [socket]);
+
+    function addFile(filePath: string) {
+        socket.emit("addFile", filePath)
+    }
+    function resetFile(filePath: string) {
+        socket.emit("resetFile", filePath)
+    }
 
     return (
         <div style={{ display: 'flex', color: 'white', height: '100vh', flexDirection: 'column' }}>
@@ -47,27 +56,28 @@ export const Git = () => {
 
             <div style={{ padding: '10px' }}>
                 <h4>Changes</h4>
-
-                {/* Unstaged Files */}
-                <div style={{ marginBottom: '20px' }}>
-                    {unstagedFiles.map((file, index) => (
-                        <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-                            <button style={{ background: 'none', border: 'none', color: 'limegreen', cursor: 'pointer' }}>
-                                +
-                            </button>
-                            <span style={{ marginLeft: '10px', color: '#cfcfcf' }}>{file}</span>
-                        </div>
-                    ))}
-                    {stagedFiles.map((file, index) => (
-                        <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-                            <button style={{ background: 'none', border: 'none', color: 'tomato', cursor: 'pointer' }}>
-                                -
-                            </button>
-                            <span style={{ marginLeft: '10px', color: '#cfcfcf' }}>{file}</span>
-                        </div>
-                    ))}
-                </div>
-
+                {!gitRunning ? (
+                    <div style={{ color: 'tomato' }}>Git is disabled</div>
+                ) : (
+                    <div style={{ marginBottom: '20px' }}>
+                {unstagedFiles.map((file, index) => (
+                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                        <button onClick={() => addFile(file)} style={{ background: 'none', border: 'none', color: 'limegreen', cursor: 'pointer' }}>
+                            +
+                        </button>
+                        <span style={{ marginLeft: '10px', color: '#cfcfcf' }}>{file}</span>
+                    </div>
+                ))}
+                {stagedFiles.map((file, index) => (
+                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                        <button onClick={() => resetFile(file)} style={{ background: 'none', border: 'none', color: 'tomato', cursor: 'pointer' }}>
+                            -
+                        </button>
+                        <span style={{ marginLeft: '10px', color: '#cfcfcf' }}>{file}</span>
+                    </div>
+                ))}
+            </div>
+                )}
             </div>
         </div>
     );

08662fff80d5e8b374222fc1c00e576f72d42e24
ExoticOnix <mshapirovskyy@gmail.com>
2024-11-16 11:47:17 +0200

    added chat tab

---
+++ frontend/app/Chat/Chat.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Chat = () => {
+
+    return (
+        <div style={{ color: 'white' }}>
+            <div style={{
+                height: '20px',
+                width: '100%',
+                backgroundColor: '#424242',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: '10px',
+                color: "white"
+            }}>
+                <span>OnixIDE</span>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Chat;
